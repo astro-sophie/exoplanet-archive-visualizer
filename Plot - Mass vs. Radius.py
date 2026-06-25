218c5f1febb33344abbcde941d2ab410b80d6e92
Yassmine <yasminmagd76@gmail.com>
2026-06-25 23:49:14 +0300

    added the interactive hover

--- Plot - Mass vs. Radius.py
+++ Plot - Mass vs. Radius.py
@@ -5,9 +5,11 @@ def create_mass_radius_plot(df, year):
 
     filtered_df = df[df['disc_year'] <= year ]
     plot_df = filtered_df.dropna(
-        subset=["pl_msinie", "pl_rade", "discoverymethod"]
+        subset=['hostname', 'pl_name', 'pl_msinie', 'pl_rade', 'disc_year', 
+         'disc_refname', 'discoverymethod', 'disc_facility', 
+         'ra', 'dec', 'gaia_dr3_id']
     )
-
+      
     fig = px.scatter(
         plot_df,
         x="pl_msinie",
@@ -18,8 +20,15 @@ def create_mass_radius_plot(df, year):
             "pl_msinie": "Mass × sin(i)",
             "pl_rade": "Planet Radius",
             "discoverymethod": "Discovery Method"
-        }
+        },
+        hover_name="pl_name", 
+        hover_data=['hostname','disc_year', 
+         'disc_refname', 'disc_facility', 
+         'ra', 'dec', 'gaia_dr3_id']
     )
+    
+    fig.update_xaxes(showspikes=True, spikecolor="green", spikesnap="cursor", spikemode="across")
+    fig.update_yaxes(showspikes=True, spikecolor="orange", spikethickness=2)
 
     fig.update_layout(
         legend_title="Discovery Method"
@@ -27,5 +36,6 @@ def create_mass_radius_plot(df, year):
 
     fig.update_xaxes(showgrid=True)
     fig.update_yaxes(showgrid=True)
-
+    
+    fig.show()
     return fig
